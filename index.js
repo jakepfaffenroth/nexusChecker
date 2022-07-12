a27ea18b55765533b7675a823fc663b7506b5d48
Jake Pfaffenroth <jake.p@wompmobile.com>
2022-07-11 19:54:55 -0700

    testing

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ if (isBetter) {
   return { shouldNotify: !!text, text };
 }
 
+
 async function notify(text) {
   const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
   const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
